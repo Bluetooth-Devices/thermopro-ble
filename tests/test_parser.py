78b0a056b7f8b2334b6b27274c6c1559c3388b24
J. Nick Koston <nick@koston.org>
2023-12-05 17:46:37 -1000

    feat: add support for TP357S (#13)
    Co-authored-by: yoxcu <mick@yoxcu.de>

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -35,6 +35,72 @@ TP357_ADD = BluetoothServiceInfo(
     source="local",
 )
 
+TP357_S = BluetoothServiceInfo(
+    name="TP357S (2142)",
+    manufacturer_data={
+        61122: b'\x00)"\x0b\x01',
+    },
+    service_uuids=[],
+    address="aa:bb:cc:dd:ee:ff",
+    rssi=-60,
+    service_data={},
+    source="local",
+)
+
+
+TP357_S_2 = BluetoothServiceInfo(
+    name="TP357S (2142)",
+    manufacturer_data={
+        61122: b'\x00)"\x0b\x01',
+        60866: b'\x00)"\x0b\x01',
+        60610: b'\x00)"\x0b\x01',
+        60354: b'\x00("\x0b\x01',
+        60098: b'\x00("\x0b\x01',
+        59842: b'\x00)"\x0b\x01',
+        59586: b'\x00("\x0b\x01',
+        59330: b'\x00("\x0b\x01',
+        59074: b'\x00("\x0b\x01',
+        58818: b'\x00("\x0b\x01',
+        58562: b"\x00'\"\x0b\x01",
+        58306: b'\x00("\x0b\x01',
+        58050: b'\x00("\x0b\x01',
+        57794: b'\x00)"\x0b\x01',
+        57538: b'\x00)"\x0b\x01',
+        57282: b'\x00)"\x0b\x01',
+        57026: b'\x00)"\x0b\x01',
+        56770: b'\x00)"\x0b\x01',
+        56514: b'\x00)"\x0b\x01',
+        56258: b'\x00)"\x0b\x01',
+        56002: b'\x00)"\x0b\x01',
+        55746: b'\x00*"\x0b\x01',
+        55490: b'\x00)"\x0b\x01',
+        55234: b'\x00*"\x0b\x01',
+        54978: b'\x00*"\x0b\x01',
+        54722: b'\x00*"\x0b\x01',
+        54466: b'\x00+"\x0b\x01',
+        54210: b'\x00-"\x0b\x01',
+        53954: b'\x00,"\x0b\x01',
+        53698: b'\x00/"\x0b\x01',
+        53442: b'\x001"\x0b\x01',
+        53186: b'\x00."\x0b\x01',
+        52930: b'\x00,"\x0b\x01',
+        52674: b'\x00,"\x0b\x01',
+        52418: b'\x00+"\x0b\x01',
+        52162: b'\x00*"\x0b\x01',
+        51906: b'\x00*"\x0b\x01',
+        51650: b'\x00*"\x0b\x01',
+        51394: b'\x00*"\x0b\x01',
+        51138: b'\x00*"\x0b\x01',
+        50882: b'\x00)"\x0b\x01',
+    },
+    service_uuids=[],
+    address="aa:bb:cc:dd:ee:ff",
+    rssi=-60,
+    service_data={},
+    source="local",
+)
+
+
 TP393 = BluetoothServiceInfo(
     name="TP393 (9376)",
     manufacturer_data={62146: b"\x005\x02,"},
@@ -147,6 +213,106 @@ def test_tp357():
                 device_key=DeviceKey(key="humidity", device_id=None),
                 name="Humidity",
                 native_value=29,
+            ),
+            DeviceKey(key="signal_strength", device_id=None): SensorValue(
+                device_key=DeviceKey(key="signal_strength", device_id=None),
+                name="Signal " "Strength",
+                native_value=-60,
+            ),
+        },
+        binary_entity_descriptions={},
+        binary_entity_values={},
+    )
+
+
+def test_tp357s():
+    parser = ThermoProBluetoothDeviceData()
+    assert parser.update(TP357_S) == SensorUpdate(
+        title="TP357S (2142) EEFF",
+        devices={
+            None: SensorDeviceInfo(
+                name="TP357S (2142)",
+                model="TP357S",
+                manufacturer="ThermoPro",
+                sw_version=None,
+                hw_version=None,
+            )
+        },
+        entity_descriptions={
+            DeviceKey(key="temperature", device_id=None): SensorDescription(
+                device_key=DeviceKey(key="temperature", device_id=None),
+                device_class=SensorDeviceClass.TEMPERATURE,
+                native_unit_of_measurement=Units.TEMP_CELSIUS,
+            ),
+            DeviceKey(key="humidity", device_id=None): SensorDescription(
+                device_key=DeviceKey(key="humidity", device_id=None),
+                device_class=SensorDeviceClass.HUMIDITY,
+                native_unit_of_measurement=Units.PERCENTAGE,
+            ),
+            DeviceKey(key="signal_strength", device_id=None): SensorDescription(
+                device_key=DeviceKey(key="signal_strength", device_id=None),
+                device_class=SensorDeviceClass.SIGNAL_STRENGTH,
+                native_unit_of_measurement=Units.SIGNAL_STRENGTH_DECIBELS_MILLIWATT,
+            ),
+        },
+        entity_values={
+            DeviceKey(key="temperature", device_id=None): SensorValue(
+                device_key=DeviceKey(key="temperature", device_id=None),
+                name="Temperature",
+                native_value=23.8,
+            ),
+            DeviceKey(key="humidity", device_id=None): SensorValue(
+                device_key=DeviceKey(key="humidity", device_id=None),
+                name="Humidity",
+                native_value=41,
+            ),
+            DeviceKey(key="signal_strength", device_id=None): SensorValue(
+                device_key=DeviceKey(key="signal_strength", device_id=None),
+                name="Signal " "Strength",
+                native_value=-60,
+            ),
+        },
+        binary_entity_descriptions={},
+        binary_entity_values={},
+    )
+    assert parser.update(TP357_S_2) == SensorUpdate(
+        title="TP357S (2142) EEFF",
+        devices={
+            None: SensorDeviceInfo(
+                name="TP357S (2142)",
+                model="TP357S",
+                manufacturer="ThermoPro",
+                sw_version=None,
+                hw_version=None,
+            )
+        },
+        entity_descriptions={
+            DeviceKey(key="temperature", device_id=None): SensorDescription(
+                device_key=DeviceKey(key="temperature", device_id=None),
+                device_class=SensorDeviceClass.TEMPERATURE,
+                native_unit_of_measurement=Units.TEMP_CELSIUS,
+            ),
+            DeviceKey(key="humidity", device_id=None): SensorDescription(
+                device_key=DeviceKey(key="humidity", device_id=None),
+                device_class=SensorDeviceClass.HUMIDITY,
+                native_unit_of_measurement=Units.PERCENTAGE,
+            ),
+            DeviceKey(key="signal_strength", device_id=None): SensorDescription(
+                device_key=DeviceKey(key="signal_strength", device_id=None),
+                device_class=SensorDeviceClass.SIGNAL_STRENGTH,
+                native_unit_of_measurement=Units.SIGNAL_STRENGTH_DECIBELS_MILLIWATT,
+            ),
+        },
+        entity_values={
+            DeviceKey(key="temperature", device_id=None): SensorValue(
+                device_key=DeviceKey(key="temperature", device_id=None),
+                name="Temperature",
+                native_value=23.8,
+            ),
+            DeviceKey(key="humidity", device_id=None): SensorValue(
+                device_key=DeviceKey(key="humidity", device_id=None),
+                name="Humidity",
+                native_value=41,
             ),
             DeviceKey(key="signal_strength", device_id=None): SensorValue(
                 device_key=DeviceKey(key="signal_strength", device_id=None),
