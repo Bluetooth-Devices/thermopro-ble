974a7489ff88c658e9755c56f0e61adb1f335cf3
J. Nick Koston <nick@koston.org>
2025-01-18 14:25:40 -1000

    chore: cleanups

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,7 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
-from typing import Any, List
+from typing import Any
 
 # -- Project information -----------------------------------------------------
 
