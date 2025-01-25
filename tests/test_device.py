20b22442746e7fa3bda5eda3eaea1280ecaf58f6
Stephan Jauernick <info@stephan-jauernick.de>
2025-01-25 23:31:40 +0100

    feat: add set_datetime service for TP358/TP393 - tests: incorporate feedback

--- tests/test_device.py
+++ tests/test_device.py
@@ -10,11 +10,6 @@ from bleak.backends.device import BLEDevice
 
 from thermopro_ble.device import ThermoProDevice
 
-# Source https://github.com/sblibs/pySwitchbot/blob/master/tests/test_adv_parser.py
-# Commit: bef9bebac1a17e8c989d343d40dca786535d149e
-# Copyright (c) 2018 Daniel Høyer Iversen
-# MIT License
-
 BLE_DEVICE_DEFAULTS = {
     "name": None,
     "rssi": -127,
@@ -44,61 +39,30 @@ def generate_ble_device(
     return BLEDevice(**new)
 
 
-# end
-
-
-class PackTestData:
-    def __init__(
-        self: PackTestData, name: str, dt: datetime, am_pm: bool, packed: bytes
-    ):
-        self.name = name
-        self.dt = dt
-        self.am_pm = am_pm
-        self.packed = packed
-
-    def test(self: PackTestData) -> tuple[bool, bytes, bytes, str]:
-        result = ThermoProDevice.pack_datetime(self.dt, self.am_pm)
-        return result == self.packed, result, self.packed, self.name
-
-    def __repr__(self: PackTestData) -> str:
-        return (
-            f"""PackTestData("{self.name}", datetime.fromisoformat"""
-            f"""("{self.dt.replace(microsecond=0).isoformat()}"), {self.am_pm}, """
-            f"""{ThermoProDevice.pack_datetime(self.dt, self.am_pm)!r}),"""
-        )
-
-    def generate(self: PackTestData) -> None:
-        print(str(self))
-
-
 now = datetime.now(tz=timezone.utc)
 
 PACK_TESTDATA = [
-    PackTestData(
-        "self-test-24hour", now, False, ThermoProDevice.pack_datetime(now, False)
-    ),
-    PackTestData(
-        "self-test-12hour", now, True, ThermoProDevice.pack_datetime(now, True)
-    ),
-    PackTestData(
+    ("self-test-24hour", now, False, ThermoProDevice.pack_datetime(now, False)),
+    ("self-test-12hour", now, True, ThermoProDevice.pack_datetime(now, True)),
+    (
         "2025-01-24-20:54:15Z-12-hour",
         datetime.fromisoformat("2025-01-24T20:54:15+00:00"),
         True,
         b"\xa5\x19\x01\x18\x146\x0f\x05\x00Z",
     ),
-    PackTestData(
+    (
         "2025-01-24-20:54:15Z-24hour",
         datetime.fromisoformat("2025-01-24T20:54:15+00:00"),
         False,
         b"\xa5\x19\x01\x18\x146\x0f\x05\x01Z",
     ),
-    PackTestData(
+    (
         "2024-03-18-10:23:46+0200-12hour",
         datetime.fromisoformat("2024-03-18T10:23:46+02:00"),
         True,
         b"\xa5\x18\x03\x12\n\x17.\x01\x00Z",
     ),
-    PackTestData(
+    (
         "2024-03-18-10:23:46+0200-24hour",
         datetime.fromisoformat("2024-03-18T10:23:46+02:00"),
         False,
@@ -108,21 +72,28 @@ PACK_TESTDATA = [
 
 
 @pytest.fixture()
+def dummy_client(monkeypatch: pytest.MonkeyPatch) -> BleakClient:
+    client = BleakClient("")
+    monkeypatch.setattr(client, "write_gatt_char", AsyncMock())
+    monkeypatch.setattr(client, "disconnect", AsyncMock())
+    return client
+
+
+@pytest.fixture()
 def dummy_device() -> ThermoProDevice:
     return ThermoProDevice(generate_ble_device("aa:bb:cc:dd:ee:ff", "TP358"))
 
 
 @pytest.fixture()
-def mock_bleak_client(monkeypatch: pytest.MonkeyPatch) -> BleakClient:
-    client = BleakClient("")
-    client.write_gatt_char = AsyncMock()
-    client.disconnect = AsyncMock()
-
+def mock_bleak_client(
+    monkeypatch: pytest.MonkeyPatch, dummy_client: BleakClient
+) -> BleakClient:
     monkeypatch.setattr(
-        "thermopro_ble.device.establish_connection", AsyncMock(return_value=client)
+        "thermopro_ble.device.establish_connection",
+        AsyncMock(return_value=dummy_client),
     )
 
-    return client
+    return dummy_client
 
 
 def test_can_create(dummy_device: ThermoProDevice) -> None:
@@ -130,22 +101,23 @@ def test_can_create(dummy_device: ThermoProDevice) -> None:
 
 
 def test_non_timezone_aware_datetime_pack() -> None:
-    with pytest.raises(ValueError) as exc_info:
+    with pytest.raises(ValueError, match="timezone aware datetime object expected"):
         ThermoProDevice.pack_datetime(
             datetime.fromisoformat("2025-01-24T20:54:15"), False
         )
 
-    assert str(exc_info.value) == "timezone aware datetime object expected", (
-        "pack_datetime should have rejected naive datetime"
-    )
 
-
-@pytest.mark.parametrize("pack_test", PACK_TESTDATA, ids=lambda val: val.name)
-def test_pack_cases(pack_test: PackTestData) -> None:
-    passed, result, packed, name = pack_test.test()
-    assert passed, (
+# to keep stability across test runs, use just the name(only str) here
+@pytest.mark.parametrize(
+    "name,dt,am_pm,packed",
+    PACK_TESTDATA,
+    ids=lambda val: val if isinstance(val, str) else "",
+)
+def test_pack_cases(name: str, dt: datetime, am_pm: bool, packed: bytes) -> None:
+    computed = ThermoProDevice.pack_datetime(dt, am_pm)
+    assert computed == packed, (
         f"test '{name} failed - expected '{packed!r}' different"
-        f"from computed '{result!r}'",
+        f"from computed '{computed!r}'",
     )
 
 
@@ -191,16 +163,16 @@ async def test_device_send_12hour(
 async def test_non_timezone_aware_datetime_set(
     mock_bleak_client: BleakClient, dummy_device: ThermoProDevice
 ) -> None:
-    with pytest.raises(ValueError) as exc_info:
+    with pytest.raises(ValueError, match="timezone aware datetime object expected"):
         await dummy_device.set_datetime(
             datetime.fromisoformat("2025-01-24T20:54:15"), True
         )
 
-    assert str(exc_info.value) == "timezone aware datetime object expected", (
-        "pack_datetime should have rejected naive datetime"
-    )
-
 
 if __name__ == "__main__":
-    for i in PACK_TESTDATA:
-        i.generate()
+    for name, dt, am_pm, packed in PACK_TESTDATA:
+        print(
+            f"('{name}', datetime.fromisoformat"
+            f"('{dt.replace(microsecond=0).isoformat()}'), {am_pm}, "
+            f"{ThermoProDevice.pack_datetime(dt, am_pm)!r}),"
+        )
